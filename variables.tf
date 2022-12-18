a4e190cb2735b209c896265711d907fd68b6a177
nickbrave777 <roed24@gmail.com>
2022-12-18 13:25:35 -0600

    Modified key variable

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "jenkins-lab-key"
+  default     =  "jenkins-lab-keys.pem"
 }
 
 variable "instance_type" {
